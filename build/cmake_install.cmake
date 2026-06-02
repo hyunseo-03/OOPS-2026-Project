6e726c696da25572cbadfaebd277c834c2b85de3
luke605 <hipong5000@gmail.com>
2026-06-03 00:31:19 +0900

    feat: Update factory view layout and UML grid

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /Users/ihyeonseo/Desktop/객지프/OOPS-2026-Project
+# Install script for directory: C:/Users/luke/Desktop/Project/OOPS-2026-Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/ImGui_Project")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,13 +34,13 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "C:/msys64/ucrt64/bin/objdump.exe")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/ihyeonseo/Desktop/객지프/OOPS-2026-Project/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/luke/Desktop/Project/OOPS-2026-Project/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/ihyeonseo/Desktop/객지프/OOPS-2026-Project/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/luke/Desktop/Project/OOPS-2026-Project/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
