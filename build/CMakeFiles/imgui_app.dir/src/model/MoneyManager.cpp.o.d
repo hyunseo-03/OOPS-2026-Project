d6dc0393ac2443102e87e9e0978737370b1b7221
teak-rim <hipong5000@gmail.com>
2026-06-03 00:13:51 +0900

    lkjlkjlk

--- build/CMakeFiles/imgui_app.dir/src/model/MoneyManager.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/imgui_app.dir/src/model/MoneyManager.cpp.o: \
-  /Users/ihyeonseo/Desktop/객지프/OOPS-2026-Project/src/model/MoneyManager.cpp \
-  /Users/ihyeonseo/Desktop/객지프/OOPS-2026-Project/include/model/MoneyManager.h
